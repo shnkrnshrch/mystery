8bec4a09029df8bc09967ec6a0525ecc3af7d741
shnkrnshrch <shnkrnshrch@users.noreply.github.com>
2019-04-12 11:42:46 -0400

    done

--- mystery.cpp
+++ mystery.cpp
@@ -26,6 +26,36 @@ void mystery1(auto& Data)
 }
 
 //... Other mysteries...
+void mystery2(auto& Data)
+{
+  cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+
+  for(int i = 0; i < Data.size(); i++)
+  {
+    for(int j = 0; j < Data.size() -1; j++)
+      if(Data[j + 1] < Data[j])
+        swap(Data[j + 1], Data[j]);
+
+    print(Data);
+  }
+
+}
+
+
+void mystery3(auto& Data)
+{
+  cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+
+  for(int i = 0; i < Data.size(); i++)
+  {
+    for(int j = i; j < Data.size() -1; j++)
+      if(Data[j] < Data[i])
+        swap(Data[i], Data[j]);
+
+    print(Data);
+  }
+
+}
 
 int main()
 {
